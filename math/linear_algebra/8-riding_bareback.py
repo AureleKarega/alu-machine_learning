cd0c7788cfa217fc7477c1ea44d7a682497c8484
AureleKarega <a.karega@alustudent.com>
2026-05-13 08:45:16 -0700

    adding 8-ridin_bareback.py

---
+++ math/linear_algebra/8-riding_bareback.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Performs matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """Returns the product of two matrices"""
+
+    # Check if matrices can be multiplied
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+
+    for i in range(len(mat1)):
+        row = []
+
+        for j in range(len(mat2[0])):
+            value = 0
+
+            for k in range(len(mat2)):
+                value += mat1[i][k] * mat2[k][j]
+
+            row.append(value)
+
+        result.append(row)
+
+    return result
