77d17cfa8401e0d910088d294c66bd3cf31efb2c
AureleKarega <a.karega@alustudent.com>
2026-05-13 08:30:59 -0700

    adding 4-line_up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Adds two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """Returns a new list with added elements"""
+    
+    if len(arr1) != len(arr2):
+        return None
+
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
