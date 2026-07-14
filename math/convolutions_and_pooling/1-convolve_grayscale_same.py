de6b3fd39392d578d1c030b5ee357f9a2077e373
Aurele Karega <a.karega@alustudent.com>
2026-07-14 09:11:36 -0700

    adding files

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,43 @@
+
+rms a same convolution on grayscale images"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a same convolution on grayscale images
+
+    Args:
+        images: numpy.ndarray with shape (m, h, w) containing multiple
+            grayscale images
+            m is the number of images
+            h is the height in pixels of the images
+            w is the width in pixels of the images
+        kernel: numpy.ndarray with shape (kh, kw) containing the kernel
+            for the convolution
+            kh is the height of the kernel
+            kw is the width of the kernel
+
+    Returns:
+        numpy.ndarray containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    pad_h = max((kh - 1) // 2, kh // 2)
+    pad_w = max((kw - 1) // 2, kw // 2)
+
+    padded_images = np.pad(
+        images,
+        ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant',
+        constant_values=0
+    )
+
+    convolved = np.zeros((m, h, w))
+
+    for i in range(h):
+        for j in range(w):
+            image_slice = padded_images[:, i:i + kh, j:j + kw]
+            convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+
+    return convolved
---
+++ math/convolutions_and_pooling/README.md
@@ -0,0 +1,21 @@
+th
+
+This project contains Python implementations of mathematical concepts and algorithms.
+
+## Description
+
+The goal of this project is to practice machine learning and mathematical programming concepts using Python and NumPy.
+
+## Files
+
+- `file_name.py` - Description of what the file does.
+
+## Requirements
+
+- Python 3
+- NumPy
+
+## Author
+
+AureleKarega
+
